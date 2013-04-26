ebf2305b1ab103e7aebd6753d57b133b3c22158a
Steven Sloan <stevenosloan@gmail.com>
2013-04-26 13:13:09 -0400

    merge tag spec

---
+++ spec/base/safe_merge_tags_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe MCMarkdown::SafeMergeTags do
+
+  it "lets things be emphasized as normal" do
+    render_string("*foo*").should == "<p><em>foo</em></p>"
+  end
+
+  it "escapes merge tags from emphasisi" do
+    render_string("*|MERGE:TAG|*").should == "<p>*|MERGE:TAG|*</p>"
+  end
+
+end
