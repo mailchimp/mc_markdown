8d2ec2dfb6375e2f02630e80f2699760fb3088da
Steven Sloan <stevenosloan@gmail.com>
2013-08-22 18:29:09 -0400

    get single line specs passing

--- lib/mc-markdown/blocks.rb
+++ lib/mc-markdown/blocks.rb
@@ -1,11 +1,15 @@
 module MCMarkdown
   module Blocks
-    BLOCK_PATTERN = /\{\{(\/?)(notes|callout)\}\}/
+    BLOCK_PATTERN = "\{\{ (\/?) (notes|callout) \}\}"
 
     # Ensure each open and close tag is on new line
     def preprocess doc
-      doc.gsub!(BLOCK_PATTERN) do |match|
-        "\n#{match}\n"
+      doc.gsub!( /(#{BLOCK_PATTERN})(.*?)(#{BLOCK_PATTERN})/mx ) do
+        open    = $1
+        content = $4.strip
+        close   = $5
+
+        "\n\n#{open}\n\n#{content}\n\n#{close}\n\n"
       end
 
       if defined?(super)
@@ -17,12 +21,14 @@ module MCMarkdown
 
     # Replace blocks enclosed in paragraphs with divs
     def postprocess doc
-      doc.gsub!(/<p>#{BLOCK_PATTERN}<\/p>/) do |match|
-        matches = match.match(BLOCK_PATTERN)
-        if matches[1] == "/"
+      doc.gsub!( /<p>#{BLOCK_PATTERN}<\/p>/x ) do
+        is_closing_tag = ($1 == "/")
+        block_type = $2
+
+        if is_closing_tag
           "</div>"
         else
-          "<div class=\"#{matches[2]}\">"
+          "<div class=\"#{block_type}\">"
         end
       end
 
