2ce761ef5b684c59d0812ba458f15dcaa0fcdb6a
Steven Sloan <stevenosloan@gmail.com>
2013-12-17 13:46:11 -0500

    pull conditional into method in HeaderWithID

--- lib/mc-markdown/formatters/header_with_id.rb
+++ lib/mc-markdown/formatters/header_with_id.rb
@@ -7,22 +7,25 @@ module MCMarkdown
       def header text, header_level
         return "<h#{header_level}>#{text}</h#{header_level}>" if header_level != 1
 
-        slug = text.strip.to_slug
-        default = "section"
+        # add ids to all h1 headers (pray they're unique)
+        if header_options.fetch(:template_tag_headers, false)
+          namespace = "{{section_id}}"
+        else
+          namespace = "#{header_options.fetch(:slug, 'section')}-#{text.strip.to_slug}"
+        end
+
+        return "<h#{header_level} id='#{namespace}'>#{text}</h#{header_level}>"
+      end
+
+      def header_options
         if defined?(extensions)
           options = extensions.fetch(:header_with_id, {})
+          options[:template_tag_headers] = extensions.fetch(:template_tag_headers, false)
         else
           options = {}
         end
 
-        # add ids to all h1 headers (pray they're unique)
-        if defined?(extensions) && extensions.fetch(:template_tag_headers, false)
-          namespace = "{{section_id}}"
-        else
-          namespace = "#{options.fetch(:slug, default)}-#{slug}"
-        end
-
-        return "<h#{header_level} id='#{namespace}'>#{text}</h#{header_level}>"
+        return options
       end
 
     end
